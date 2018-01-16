65d2b8d63c7c74fbeeaf1b64c832dbe13f6e9ac9
JelleDekkers <jelle.dekkers@hotmail.com>
2018-01-16 19:36:44 +0100

    Removed redundant audio listeners from the pet prefabs and fixed a faulty referencing.

--- HoloPetUnity/Assets/Scripts/Pet.cs
+++ HoloPetUnity/Assets/Scripts/Pet.cs
@@ -4,9 +4,5 @@ using UnityEngine;
 
 public class Pet : MonoBehaviour {
 
-    public PetHead Head { get; private set; }
-
-    private void Start() {
-        Head = transform.GetChild(0).GetComponent<PetHead>();
-    }
+    public PetHead Head;
 }
